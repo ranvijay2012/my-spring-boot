e7bf02f3ca2fc373c75e5869a24727db037f1761
Ranvijay Singh <ranvijsysingh2012@gmail.com>
2025-05-13 21:21:32 +0530

    modify in bean level

--- src/main/java/com/spring/boot/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/spring/boot/service/impl/EmployeeServiceImpl.java
@@ -26,33 +26,44 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<EmployeeDto> getEmployees() {
+        log.info("Getting employee list from DB ");
         List<Employee> employees = employeeRepository.findAll();
+        log.info("Now converting employee entity into dto");
         List<EmployeeDto> employeeDtos = employeeAdopter.mapOneTypeListToAnotherType(employees, EmployeeDto.class);
+        log.info("After converting entity to dto then return a dto list with size: {}", employeeDtos.size());
         return employeeDtos;
     }
 
     @Override
     public EmployeeDto getEmployee(Long empId) {
+        log.info("Getting employee detail from DB with ID: {}", empId);
         Optional<Employee> optionalEmployee = employeeRepository.findById(empId);
+        log.info("Fetched employee detail successfully from DB");
         Employee employee = optionalEmployee.orElseGet(Employee::new);
+        log.info("Map employee entity to dto and return data with ID: {}", empId);
         return (EmployeeDto) employeeAdopter.mapOneObjectToAnother(employee, EmployeeDto.class);
     }
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) throws ApplicationException {
-        log.info("Now going to save data is: {}", employeeDto);
+        log.info("Map employee dto into entity data is: {}", employeeDto);
         Employee employee = (Employee) employeeAdopter.mapOneObjectToAnother(employeeDto, Employee.class);
         try {
+            log.info("Employee data saving in DB");
             employee = employeeRepository.save(employee);
+            log.info("Employee data saved successfully in DB with id: {}", employee.getId());
         } catch (Exception ex) {
             log.error("Employee data could not be save due to {}", ex.getMessage());
             throw new ApplicationException(ErrorDetails.EMPLOYEE_DATA_UNABLE_TO_SAVE, ex);
         }
+        log.info("Now converting entity to dto and return a dto");
         return (EmployeeDto) employeeAdopter.mapOneObjectToAnother(employee, EmployeeDto.class);
     }
 
     @Override
     public void deleteEmployee(Long empId) {
+        log.info("Deleting employee detail from DB with ID: {}", empId);
         employeeRepository.deleteById(empId);
+        log.info("Deleted employee detail from DB successfully");
     }
 }
