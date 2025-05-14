d005e1e3df8c969a40e24034a14783c83f3004de
Ranvijay Singh <ranvijsysingh2012@gmail.com>
2025-05-14 15:10:24 +0530

    modify in bean level

--- src/main/java/com/spring/boot/SpringBootApp.java
+++ src/main/java/com/spring/boot/SpringBootApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EntityScan(basePackages = "com.spring.boot")
 @EnableJpaRepositories
+@EnableFeignClients
 public class SpringBootApp implements CommandLineRunner {
 
     public static void main( String[] args ) {
